0711cded2c7dd3549bba0369ee67f07056e7d7b1
Sid-003 <siddhartha4030@gmail.com>
2018-09-30 17:17:08 -0400

    added IsOwner attribute

---
+++ Rethought.Commands.Discord.Net/Conditions/IsOwner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rethought.Commands.Discord.Net.Conditions
+{
+    public class IsOwner : IAsyncCondition<DiscordContext>
+    {
+        public async Task<bool> SatisfiedAsync(DiscordContext context)
+        {
+            var application = await context.Client.GetApplicationInfoAsync();
+            if (context.User.Id != application.Owner.Id)
+                return false;
+            return true;
+        }
+    }
+}
