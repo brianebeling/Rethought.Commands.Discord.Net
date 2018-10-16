afe40480c2a0cd72c43c28a29ecce2caad198e3e
Brian Ebeling <bje@live.de>
2018-10-16 20:42:52 +0200

    Fix compiling error and change to .Net Standard 2.0

---
+++ Rethought.Commands.Discord.Net/Conditions/IsOwnerCondition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Rethought.Commands.Conditions;
+
+namespace Rethought.Commands.Discord.Net.Conditions
+{
+    public class IsOwnerCondition : IAsyncCondition<DiscordContext>
+    {
+        public async Task<bool> SatisfiedAsync(DiscordContext context)
+        {
+            var application = await context.Client.GetApplicationInfoAsync().ConfigureAwait(false);
+            
+            return context.User.Id == application.Owner.Id;
+        }
+    }
+}
